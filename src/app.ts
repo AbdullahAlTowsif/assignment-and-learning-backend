14e6367010d1bcc9b27826e8e58488535ebd5066
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-04-23 20:40:43 +0600

    prisma added

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { envVars } from './app/config/env';
+
+const app: Application = express();
+
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+}));
+
+//parser
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        message: "Server is running...😃",
+        environment: envVars.NODE_ENV,
+        uptime: process.uptime().toFixed(2) + " sec",
+        timeStamp: new Date().toISOString()
+    })
+});
+
+
+export default app;
---
+++ src/app/config/env.ts
@@ -0,0 +1,73 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EnvConfig {
+    PORT: string;
+    DATABASE_URL: string;
+    NODE_ENV: "development" | "production";
+    // BCRYPT_SALT_ROUND: string;
+    // JWT_ACCESS_EXPIRES: string;
+    // JWT_ACCESS_SECRET: string;
+    // JWT_REFRESH_SECRET: string;
+    // JWT_REFRESH_EXPIRES: string;
+    // ADMIN_EMAIL: string;
+    // ADMIN_PASSWORD: string;
+    // EXPRESS_SESSION_SECRET: string;
+    // FRONTEND_URL: string;
+    // CLOUDINARY_API_SECRET: string;
+    // CLOUDINARY_API_KEY: string;
+    // CLOUDINARY_CLOUD_NAME: string;
+    // STRIPE_SECRET_KEY: string;
+    // OPENROUTER_API_KEY: string;
+}
+
+const loadEnvVariables = (): EnvConfig => {
+    const requiredEnvVariables: string[] = [
+        "PORT",
+        "DATABASE_URL",
+        "NODE_ENV",
+        // "BCRYPT_SALT_ROUND",
+        // "JWT_ACCESS_EXPIRES",
+        // "JWT_ACCESS_SECRET",
+        // "ADMIN_EMAIL",
+        // "ADMIN_PASSWORD",
+        // "JWT_REFRESH_SECRET",
+        // "JWT_REFRESH_EXPIRES",
+        // "EXPRESS_SESSION_SECRET",
+        // "FRONTEND_URL",
+        // "CLOUDINARY_API_SECRET",
+        // "CLOUDINARY_API_KEY",
+        // "CLOUDINARY_CLOUD_NAME",
+        // "STRIPE_SECRET_KEY",
+        // "OPENROUTER_API_KEY",
+    ];
+
+    requiredEnvVariables.forEach((key) => {
+        if (!process.env[key]) {
+            throw new Error(`Missing required environment variable ${key}`);
+        }
+    });
+    return {
+        PORT: process.env.PORT as string,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        DATABASE_URL: process.env.DATABASE_URL!,
+        NODE_ENV: process.env.NODE_ENV as "development" | "production",
+        // BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+        // JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        // JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        // JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        // JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+        // ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
+        // ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
+        // EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+        // FRONTEND_URL: process.env.FRONTEND_URL as string,
+        // CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+        // CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+        // CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+        // STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY as string,
+        // OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY as string,
+    };
+};
+
+export const envVars = loadEnvVariables();
